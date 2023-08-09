f7f99eb9d5ab66f6d6c004da4ebac4bf959ba8e5
Solonchenko Ivan <vanek.solonchenko@gmail.com>
2023-08-09 19:50:39 +0300

    first merge main into ivan

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,25 @@
-
 import './App.css';
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import Appointments from './components/appointments/index';
-import Doctors from './components/appointments/doctors';
 
 function App() {
-  return (
-    
-    <div >
-      <div>
-      <Doctors />   
-        <a href="doctors" target="_blanc">Doctors</a>
-      </div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/appointments/:id' element={<Appointments/>} />
-      </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+                <Route path='/appointments/:id' element={<Appointments/>} />
+              </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ app/src/components/Login/index.js
@@ -0,0 +1,85 @@
+import { Form, Input } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { message } from 'antd';
+
+function Login() {
+    const navigate = useNavigate();
+    const server = 'https://jsonplaceholder.typicode.com/users';
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    useEffect(() => {
+        const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+        if (loggedInUser) navigate("/home");
+    }, [navigate]);
+
+    const onFinish = (values) => {
+        fetch(server)
+            .then(response => response.json())
+            .then(users => {
+                const foundUser = users.find(user => user.email === values.email && user.username === values.password);
+                const errorPassword = users.find(user => user.email === values.email && user.username !== values.password);
+
+                if (foundUser) {
+                    message.success('Ви увійшли!');
+                    localStorage.setItem('loggedInUser', JSON.stringify(values));
+                    navigate('/home');
+                    clearFields();
+                } else if (errorPassword) {
+                    message.error('Ви ввели невірний пароль!');
+                    setFormData({ ...formData, password: '' });
+                } else {
+                    message.error('Користувача з таким email та паролем не знайдено. Зареєструйтесь!');
+                    navigate('/register');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
+
+    const handleInputChange = e => {
+        const { name, value } = e.target;
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const clearFields = () => {
+        setFormData({
+            email: '',
+            password: '',
+        });
+    };
+
+    return (
+        <div className="login-content">
+            <Form layout="vertical" className="login-form" onFinish={onFinish}>
+                <h2 className="login-form-title">Вхід
+                    <hr />
+                </h2>
+                <Form.Item label="Email" className={"no-star"} name="email" rules={[{ required: true, message: 'Будь ласка, введіть email' }]}>
+                    <Input type="email"  name="email" value={formData.email} onChange={handleInputChange} />
+                </Form.Item>
+                <Form.Item label="Пароль" className={"no-star"} name="password" rules={[{ required: true, message: 'Будь ласка, введіть пароль' }]}>
+                    <Input type="password" name="password" value={formData.password} onChange={handleInputChange} />
+                </Form.Item>
+                <Form.Item>
+                    <button className="login-button" type="submit">
+                        Увійти
+                    </button>
+                </Form.Item>
+                <Link to="/register">
+                    Не маєте облікового запису? <strong>Зареєструватися</strong>
+                </Link>
+            </Form>
+        </div>
+    );
+}
+
+export default Login;
