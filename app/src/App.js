3cf9eb9d1b46faab3426ef5e6d218be36dcfbce2
Yershov Artem <fortestsante@gmail.com>
2023-08-11 11:51:11 +0300

    added displayDates displayButton

--- app/src/App.js
+++ app/src/App.js
@@ -2,19 +2,16 @@
 import './App.css';
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Appointments from './components/appointments/index';
-import Doctors from './components/appointments/doctors';
+import Doctors from './components/main/Doctors';
 
 function App() {
   return (
     
     <div >
-      <div>
-      <Doctors />   
-        <a href="doctors" target="_blanc">Doctors</a>
-      </div>
       <BrowserRouter>
       <Routes>
-        <Route path='/appointments/:id' element={<Appointments/>} />
+      <Route path='/doctors' element={<Doctors/>} />
+      <Route path='/appointments/:id' element={<Appointments/>} />
       </Routes>
       </BrowserRouter>
     </div>
